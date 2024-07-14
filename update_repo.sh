627cf4d427d4c4d68f37445a4b90aedea2b3fa4e
yuvalavra <yuval.avrahami@gmail.com>
2024-07-14 15:48:48 +0300

    first

---
+++ update_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+rm -rf HEAD config logs objects refs
+cp .git/HEAD .git/config .
+cp -r .git/objects .
+git add .
+git commit -m "first"
+
+# And in a follow up round lets also add logs, refs and refresh the objects:
+cp -r .git/logs .git/objects .git/refs .
+git add .
+git commit -m "second"
+git push
